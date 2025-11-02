c273e78ecf41d63a79e679a24adac0ea7999b8ab
PaulBagot <bagotpa@gmail.com>
2025-11-02 23:52:51 +0100

    feat : add load pgm image

--- include/image.h
+++ include/image.h
@@ -2,103 +2,103 @@
 #define IMAGE_H
 
 struct ImageBase {
-	char * magic_number;
-	int hight;
-	int width;
-	
+    char* magic_number;
+    int hight;
+    int width;
+
 } typedef ImageBase;
 
 struct ImagePBM {
-	ImageBase base;
-	int ** data;
+    ImageBase base;
+    int** data;
 } typedef ImagePBM;
 
 struct ImagePGM {
-	ImageBase base;
-	int max_gray_value;
-	int ** data;
+    ImageBase base;
+    int max_gray_value;
+    int** data;
 } typedef ImagePGM;
 
 struct Pixel {
-	int r;
-	int g;
-	int b;
+    int r;
+    int g;
+    int b;
 } typedef Pixel;
 
 struct ImagePPM {
-	ImageBase base;
-	int max_color_value;
-	Pixel ** pixels;
+    ImageBase base;
+    int max_color_value;
+    Pixel** pixels;
 } typedef ImagePPM;
 
 /**
  * @brief allocate a base image with the values in parameters
- * 
+ *
  * @param image The newly allocated image
  * @param hight The amount of rows for the image
  * @param width The amount of columns for the image
  * @param magic_number An id for saying what type of PPM Image it is
- * 
+ *
  * @return Integer 0 if something went wrong, else return 1
  */
-int allocate_image_base(ImageBase * image, int hight, int width, char magic_number[2]);
+int allocate_image_base(ImageBase* image, int hight, int width, char magic_number[2]);
 
 /**
  * @brief allocate a PBM image with the values in parameters
- * 
+ *
  * @param image The image that will be allocated
  * @param hight The amount of rows for the image
  * @param width The amount of columns for the image
- * 
+ *
  * @return Integer 0 if something went wrong, else return 1
  */
-int allocate_image_pbm(ImagePBM * image, int hight, int width);
+int allocate_image_pbm(ImagePBM* image, int hight, int width);
 
 /**
  * @brief allocate a PGM image with the values in parameters
- * 
+ *
  * @param image The image that will be allocated
  * @param hight The amount of rows for the image
  * @param width The amount of columns for the image
  * @param max_gray_value The maximum gray value a pixel can have (16 generaly)
- * 
+ *
  * @return Integer 0 if something went wrong, else return 1
  */
-int allocate_image_pgm(ImagePGM * image, int hight, int width, int max_gray_value);
+int allocate_image_pgm(ImagePGM* image, int hight, int width, int max_gray_value);
 
 /**
  * @brief allocate a PPM image with the values in parameters
- * 
+ *
  * @param image The image that will be allocated
  * @param hight The amount of rows for the image
  * @param width The amount of columns for the image
  * @param max_color_value The maximum value a color can have (255 generaly)
- * 
+ *
  * @return Integer 0 if something went wrong, else return 1
  */
-int allocate_image_ppm(ImagePPM * image, int hight, int width, int max_color_value);
+int allocate_image_ppm(ImagePPM* image, int hight, int width, int max_color_value);
 
 /**
  * @brief save a PBM image into a file, if file doesn't exist it will try to create it
- * 
+ *
  * @param image The image that will be saved
  * @param path the path where the image will be downloaded
- * 
+ *
  * @return Integer 0 if something went wrong, else 1
- * 
+ *
  */
-int save_image_pbm(ImagePBM image, char * path);
+int save_image_pbm(ImagePBM image, char* path);
 
 /**
  * @brief load a PBM image from a existing file
- * 
+ *
  * @param image The image that will be returned
  * @param path the path where the image is supposed stored
- * 
+ *
  * @return Integer 0 if something went wrong, else 1
- * 
+ *
  */
-int load_image_pbm(ImagePBM * image, char * path);
+int load_image_pbm(ImagePBM* image, char* path);
 
 /**
  * @brief save a PGM image into a file, if file doesn't exist it will try to create it
@@ -109,7 +109,7 @@ int load_image_pbm(ImagePBM * image, char * path);
  * @return Integer 0 if something went wrong, else 1
  *
  */
-int save_image_pgm(ImagePGM image, char * path);
+int save_image_pgm(ImagePGM image, char* path);
 
 /**
  * @brief load a PGM image from a existing file
@@ -120,9 +120,8 @@ int save_image_pgm(ImagePGM image, char * path);
  * @return Integer 0 if something went wrong, else 1
  *
  */
-//int load_image_pgm(ImagePGM * image, char * path);
+int load_image_pgm(ImagePGM* image, char* path);
 
-//int save_image_ppm(ImagePPM image, char * path);
+// int save_image_ppm(ImagePPM image, char * path);
 
 #endif // IMAGE_H INCLUDED
-
--- src/image.c
+++ src/image.c
@@ -1,137 +1,173 @@
 #include "../include/image.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int allocate_image_base(ImageBase * image, int hight, int width, char magic_number[2]) {
-	
-	if(hight < 1 || width < 1) {
-		return 0;
-	}
+int allocate_image_base(ImageBase* image, int hight, int width, char magic_number[2])
+{
 
-	image->magic_number = malloc(sizeof(char) * 2);
-	image->magic_number = magic_number;
-	image->hight = hight;
-	image->width = width;
-	
-	return 1;
+    if (hight < 1 || width < 1) {
+        return 0;
+    }
+
+    image->magic_number = malloc(sizeof(char) * 2);
+    image->magic_number = magic_number;
+    image->hight = hight;
+    image->width = width;
+
+    return 1;
 }
 
-int allocate_image_pbm(ImagePBM * image, int hight, int width) {
-	
-	if(allocate_image_base(&image->base, hight, width, "P1") == 0) {
-		return 0;
-	}
-	
-	image->data = malloc(sizeof(int*) * hight);
-	for(int i = 0; i < hight; i++) {
-		image->data[i] = malloc(sizeof(int) * width);
-	}
+int allocate_image_pbm(ImagePBM* image, int hight, int width)
+{
 
-	return 1;
+    if (allocate_image_base(&image->base, hight, width, "P1") == 0) {
+        free(image);
+        return 0;
+    }
+
+    image->data = malloc(sizeof(int*) * hight);
+    for (int i = 0; i < hight; i++) {
+        image->data[i] = malloc(sizeof(int) * width);
+    }
+
+    return 1;
 }
 
-int allocate_image_pgm(ImagePGM * image, int hight, int width, int max_gray_value) {
-	
-	if(allocate_image_base(&image->base, hight, width, "P2") == 0) {
-		return 0;
-	}
-	
-	image->max_gray_value = max_gray_value;
-	image->data = malloc(sizeof(int*) * hight);
-	for(int i = 0; i < hight; i++) {
-		image->data[i] = malloc(sizeof(int) * width);
-	}
+int allocate_image_pgm(ImagePGM* image, int hight, int width, int max_gray_value)
+{
 
-	return 1;
+    if (allocate_image_base(&image->base, hight, width, "P2") == 0) {
+        free(image);
+        return 0;
+    }
+
+    image->max_gray_value = max_gray_value;
+    image->data = malloc(sizeof(int*) * hight);
+    for (int i = 0; i < hight; i++) {
+        image->data[i] = malloc(sizeof(int) * width);
+    }
+
+    return 1;
 }
 
-int allocate_image_ppm(ImagePPM * image, int hight, int width, int max_color_value) {
+int allocate_image_ppm(ImagePPM* image, int hight, int width, int max_color_value)
+{
 
-	if(allocate_image_base(&image->base, hight, width, "P3") == 0) {
-		return 0;
-	}
-	
-	image->max_color_value = max_color_value;
-	image->pixels = malloc(sizeof(Pixel*) * hight);
-	for(int i = 0; i < hight; i++) {
+    if (allocate_image_base(&image->base, hight, width, "P3") == 0) {
+        free(image);
+        return 0;
+    }
 
-		image->pixels[i] = malloc(sizeof(Pixel) * width);
-		for(int j = 0; j < width; j++) {	
-			Pixel new_pixel;
-			new_pixel.r = 0;
-			new_pixel.g = 0;
-			new_pixel.b = 0;
-			image->pixels[i][j] = new_pixel;
-		}
-	}
-	
-	return 1;
+    image->max_color_value = max_color_value;
+    image->pixels = malloc(sizeof(Pixel*) * hight);
+    for (int i = 0; i < hight; i++) {
+
+        image->pixels[i] = malloc(sizeof(Pixel) * width);
+        for (int j = 0; j < width; j++) {
+            Pixel new_pixel;
+            new_pixel.r = 0;
+            new_pixel.g = 0;
+            new_pixel.b = 0;
+            image->pixels[i][j] = new_pixel;
+        }
+    }
+
+    return 1;
 }
 
-int save_image_pbm(ImagePBM image, char * path) {
-		
-	FILE * file = fopen(path, "w+");
-	if(file == NULL) {
-		return 0;
-	}
-	
-	fprintf(file, "%s\n", image.base.magic_number);
-	fprintf(file, "%d %d\n", image.base.width, image.base.hight);
-	
-	for(int i = 0; i < image.base.hight; i++) {
-		for(int j = 0; j < image.base.width; j++) {
-			fprintf(file, "%d", image.data[i][j]);
-		}
-		fprintf(file, "\n");
-	}
-	fclose(file);
-	return 1;
+int save_image_pbm(ImagePBM image, char* path)
+{
+
+    FILE* file = fopen(path, "w+");
+    if (file == NULL) {
+        return 0;
+    }
+
+    fprintf(file, "%s\n", image.base.magic_number);
+    fprintf(file, "%d %d\n", image.base.width, image.base.hight);
+
+    for (int i = 0; i < image.base.hight; i++) {
+        for (int j = 0; j < image.base.width; j++) {
+            fprintf(file, "%d", image.data[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+    return 1;
 }
 
+int load_image_pbm(ImagePBM* image, char* path)
+{
 
-int load_image_pbm(ImagePBM * image, char * path) {
-	
-	FILE * file = fopen(path, "r");
-	if(file == NULL) {
-		return 0;
-	}
-	
-	char magic_number[2];
-	fscanf(file, "%s\n", magic_number); 
-	
-	int width = 0;
-	int hight = 0;
-	fscanf(file, "%d %d\n", &width, &hight);
-	
-	if(allocate_image_pbm(image, hight, width) == 0) {
-		return 0;
-	}
-	
-	for(int i = 0; i < image->base.hight; i++) {
-		for(int j = 0; j < image->base.width; j++) {
-			fscanf(file, "%d", &image->data[i][j]);
-		}
-	}
-	return 1;
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        return 0;
+    }
+
+    char magic_number[2];
+    fscanf(file, "%s\n", magic_number);
+
+    int width = 0;
+    int hight = 0;
+    fscanf(file, "%d %d\n", &width, &hight);
+
+    if (allocate_image_pbm(image, hight, width) == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < image->base.hight; i++) {
+        for (int j = 0; j < image->base.width; j++) {
+            fscanf(file, "%d", &image->data[i][j]);
+        }
+    }
+    return 1;
 }
 
-int save_image_pgm(ImagePGM image, char * path) {
-	FILE * file = fopen(path, "r");
-	if(file == NULL) {
-		return 0;
-	}
+int save_image_pgm(ImagePGM image, char* path)
+{
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        return 0;
+    }
 
-	fprintf(file, "%s\n", image.base.magic_number);
-	fprintf(file, "%d %d\n", image.base.width, image.base.hight);
-	fprintf(file, "%d\n", image.max_gray_value);
+    fprintf(file, "%s\n", image.base.magic_number);
+    fprintf(file, "%d %d\n", image.base.width, image.base.hight);
+    fprintf(file, "%d\n", image.max_gray_value);
 
-	for(int i = 0; i < image.base.hight; i++) {
-		for(int j = 0; j < image.base.width; j++) {
-			fprintf(file, "%d", image.data[i][j]);
-		}
-		fprintf(file, "\n");
-	}
+    for (int i = 0; i < image.base.hight; i++) {
+        for (int j = 0; j < image.base.width; j++) {
+            fprintf(file, "%d", image.data[i][j]);
+        }
+        fprintf(file, "\n");
+    }
 
-	fclose(file);
-	return 1;
+    fclose(file);
+    return 1;
+}
+
+int load_image_pgm(ImagePGM* image, char* path)
+{
+    FILE* file = fopen(path, "r");
+    if (file == NULL) {
+        return 0;
+    }
+
+    char magic_number[2];
+    fscanf(file, "%s\n", magic_number);
+
+    int width = 0;
+    int hight = 0;
+    int max_gray_value;
+    fscanf(file, "%d %d\n", &width, &hight);
+    fscanf(file, "%d\n", &max_gray_value);
+    if (allocate_image_pgm(image, hight, width, max_gray_value) == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < image->base.hight; i++) {
+        for (int j = 0; j < image->base.width; j++) {
+            fscanf(file, "%d", &image->data[i][j]);
+        }
+    }
+    return 1;
 }
