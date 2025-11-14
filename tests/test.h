820d4de5f35ba1352f575244d60f919b4ebbeaa2
PaulBagot <bagotpa@gmail.com>
2025-11-14 18:10:31 +0100

    tests : refactor tests with a framework

--- tests/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-void display_start_of_test(char * texte);
-void display_end_of_test();
-
-#endif // TEST_H INCLUDED
--- tests/test_image.c
+++ tests/test_image.c
@@ -1,29 +1,34 @@
 #include "../include/image.h"
-#include "test.h"
-#include <stdio.h>
-#include <string.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <CUnit/TestRun.h>
+#include <CUnit/TestDB.h>
 
 void test_allocate_image_base()
 {
-    printf("#%s\n", __func__);
     ImageBase base;
-    printf("%d\n", allocate_image_base(&base, 0, 0, "P1") == 0);
-    printf("%d\n", allocate_image_base(&base, 0, 1, "P1") == 0);
-    printf("%d\n", allocate_image_base(&base, 1, -1, "P1") == 0);
-    printf("%d\n", allocate_image_base(&base, 150, 140, "P1") == 1);
-    printf("%d\n", base.hight == 150);
-    printf("%d\n", base.width == 140);
-    printf("%d\n", strcmp(base.magic_number, "P1") == 0);
+
+    CU_ASSERT_FALSE(allocate_image_base(&base, 0, 0, "P1"));
+    CU_ASSERT_FALSE(allocate_image_base(&base, 0, 1, "P1"));
+    CU_ASSERT_FALSE(allocate_image_base(&base, 1, -1, "P1"));
+    CU_ASSERT_TRUE(allocate_image_base(&base, 150, 140, "P1"));
+
+    CU_ASSERT(base.hight == 150);
+    CU_ASSERT(base.width == 140);
+
+    CU_ASSERT_STRING_EQUAL(base.magic_number, "P1");
 }
 
 void test_allocate_image_pbm()
 {
-    printf("#%s\n", __func__);
     ImagePBM image;
-    printf("%d\n", allocate_image_pbm(&image, 150, 140) == 1);
-    printf("%d\n", image.base.hight == 150);
-    printf("%d\n", image.base.width == 140);
-    printf("%d\n", strcmp(image.base.magic_number, "P1") == 0);
+
+    CU_ASSERT_TRUE(allocate_image_pbm(&image, 150, 140));
+    CU_ASSERT(image.base.hight == 150);
+    CU_ASSERT(image.base.width == 140);
+
+    CU_ASSERT_STRING_EQUAL(image.base.magic_number, "P1");
+
     int test = 1;
     for (int i = 0; i < image.base.hight; i++) {
         for (int j = 0; j < image.base.width; j++) {
@@ -32,18 +37,21 @@ void test_allocate_image_pbm()
             }
         }
     }
-    printf("%d\n", test);
+    CU_ASSERT_TRUE(test);
 }
 
 void test_allocate_image_pgm()
 {
-    printf("#%s\n", __func__);
     ImagePGM image;
-    printf("%d\n", allocate_image_pgm(&image, 100, 50, 255) == 1);
-    printf("%d\n", image.base.hight == 100);
-    printf("%d\n", image.base.width == 50);
-    printf("%d\n", strcmp(image.base.magic_number, "P2") == 0);
-    printf("%d\n", image.max_gray_value == 255);
+
+    CU_ASSERT_TRUE(allocate_image_pgm(&image, 100, 50, 255));
+
+    CU_ASSERT(image.base.hight == 100);
+    CU_ASSERT(image.base.width == 50);
+
+    CU_ASSERT_STRING_EQUAL(image.base.magic_number, "P2");
+    CU_ASSERT(image.max_gray_value == 255);
+
     int test = 1;
     for (int i = 0; i < image.base.hight; i++) {
         for (int j = 0; j < image.base.width; j++) {
@@ -52,18 +60,19 @@ void test_allocate_image_pgm()
             }
         }
     }
-    printf("%d\n", test);
+    CU_ASSERT_TRUE(test);
 }
 
 void test_allocate_image_ppm()
 {
-    printf("#%s\n", __func__);
     ImagePPM image;
-    printf("%d\n", allocate_image_ppm(&image, 50, 100, 255) == 1);
-    printf("%d\n", image.base.hight == 50);
-    printf("%d\n", image.base.width == 100);
-    printf("%d\n", strcmp(image.base.magic_number, "P3") == 0);
-    printf("%d\n", image.max_color_value == 255);
+
+    CU_ASSERT_TRUE(allocate_image_ppm(&image, 50, 100, 255));
+    CU_ASSERT(image.base.hight == 50);
+    CU_ASSERT(image.base.width == 100);
+    CU_ASSERT_STRING_EQUAL(image.base.magic_number, "P3");
+    CU_ASSERT(image.max_color_value == 255);
+
     int test = 1;
     for (int i = 0; i < image.base.hight; i++) {
         for (int j = 0; j < image.base.width; j++) {
@@ -78,12 +87,11 @@ void test_allocate_image_ppm()
             }
         }
     }
-    printf("%d\n", test);
+    CU_ASSERT_TRUE(test);
 }
 
 void test_save_image_pbm()
 {
-    printf("#%s\n", __func__);
     ImagePBM image;
     allocate_image_pbm(&image, 3, 3);
     for (int i = 0; i < 3; i++) {
@@ -91,12 +99,11 @@ void test_save_image_pbm()
             image.data[i][j] = i % 2;
         }
     }
-    printf("%d\n", save_image_pbm(image, "./tests/tmp/test_save_pbm.ppm") == 1);
+    CU_ASSERT_TRUE(save_image_pbm(image, "./tests/tmp/test_save_pbm.ppm"));
 }
 
 void test_save_image_pgm()
 {
-    printf("#%s\n", __func__);
     ImagePGM image;
     allocate_image_pgm(&image, 3, 3, 15);
     for (int i = 0; i < 3; i++) {
@@ -104,30 +111,28 @@ void test_save_image_pgm()
             image.data[i][j] = i * j;
         }
     }
-    printf("%d\n", save_image_pgm(image, "./tests/tmp/test_save_pgm.ppm") == 1);
+    CU_ASSERT_TRUE(save_image_pgm(image, "./tests/tmp/test_save_pgm.ppm"));
 }
 
 void test_create_pixel()
 {
-    printf("#%s\n", __func__);
     ImagePPM image;
     allocate_image_ppm(&image, 1, 1, 255);
     int max = image.max_color_value;
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, -1, 0, 0) == 0);
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, 0, -1, 0) == 0);
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, 0, 0, -1) == 0);
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, -1, 0, 0));
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, 0, -1, 0));
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, 0, 0, -1));
 
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, max + 1, max, max) == 0);
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, max, max + 1, max) == 0);
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, max, max, max + 1) == 0);
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, max + 1, max, max));
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, max, max + 1, max));
+    CU_ASSERT_FALSE(create_pixel(&image.pixels[0][0], max, max, max, max + 1));
 
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, 0, 0, 0) == 1);
-    printf("%d\n", create_pixel(&image.pixels[0][0], max, max, max, max) == 1);
+    CU_ASSERT_TRUE(create_pixel(&image.pixels[0][0], max, 0, 0, 0));
+    CU_ASSERT_TRUE(create_pixel(&image.pixels[0][0], max, max, max, max));
 }
 
 void test_save_image_ppm()
 {
-    printf("#%s\n", __func__);
     ImagePPM image;
     allocate_image_ppm(&image, 3, 3, 255);
 
@@ -143,20 +148,29 @@ void test_save_image_ppm()
     create_pixel(&image.pixels[2][1], image.max_color_value, 255, 255, 255);
     create_pixel(&image.pixels[2][2], image.max_color_value, 0, 0, 0);
 
-    printf("%d\n", save_image_ppm(image, "./tests/tmp/test_save_ppm.ppm") == 1);
+    CU_ASSERT_TRUE(save_image_ppm(image, "./tests/tmp/test_save_ppm.ppm"));
 }
 
 int main(void)
 {
-    display_start_of_test("tests image");
-    test_allocate_image_base();
-    test_allocate_image_pbm();
-    test_allocate_image_pgm();
-    test_allocate_image_ppm();
-    test_create_pixel();
-    test_save_image_pbm();
-    test_save_image_pgm();
-    test_save_image_ppm();
-    display_end_of_test();
+    CU_initialize_registry();
+
+    CU_pSuite suiteAllocate = CU_add_suite("Tests Images Allocations", NULL, NULL);
+    CU_add_test(suiteAllocate, "test_allocate_image_base", test_allocate_image_base);
+    CU_add_test(suiteAllocate, "test_allocate_image_pbm", test_allocate_image_pbm);
+    CU_add_test(suiteAllocate, "test_allocate_image_pgm", test_allocate_image_pgm);
+    CU_add_test(suiteAllocate, "test_allocate_image_ppm", test_allocate_image_ppm);
+
+    CU_pSuite suiteSave = CU_add_suite("Tests Images Savings", NULL, NULL);
+    CU_add_test(suiteSave, "test_save_image_pbm", test_save_image_pbm);
+    CU_add_test(suiteSave, "test_save_image_pgm", test_save_image_pgm);
+    CU_add_test(suiteSave, "test_create_pixel", test_create_pixel);
+    CU_add_test(suiteSave, "test_save_image_ppm", test_save_image_ppm);
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+    CU_cleanup_registry();
+
     return 0;
 }
